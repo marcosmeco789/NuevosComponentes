e2fb430649d3ef494133d2a48bd97d7272d890d6
marcosmeco789 <marcosmeco987@gmail.com>
2025-03-06 12:37:48 +0100

    06/03

---
+++ Ej5/ValidateTextBox.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace Ej5
+{
+    partial class ValidateTextBox
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txt = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txt
+            // 
+            this.txt.Location = new System.Drawing.Point(10, 10);
+            this.txt.Name = "txt";
+            this.txt.Size = new System.Drawing.Size(100, 20);
+            this.txt.TabIndex = 0;
+            // 
+            // ValidateTextBox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txt);
+            this.Name = "ValidateTextBox";
+            this.Size = new System.Drawing.Size(124, 46);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txt;
+    }
+}
---
+++ Ej5/ValidateTextBox.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Ej5
+{
+    public partial class ValidateTextBox: UserControl
+    {
+        public ValidateTextBox()
+        {
+            InitializeComponent();
+            IniciarTextBox();
+        }
+
+        private void IniciarTextBox()
+        {
+            txt.Location = new Point(10, 10);
+            this.Height = txt.Height + 20;
+            txt.Width = this.Width - 20;
+
+        }
+
+
+        [Category("Appearance")]
+        [Description("blabla")]
+
+        public string TxtText
+        {
+            set
+            {
+                txt.Text = value;
+            }
+            get
+            {
+                return txt.Text;
+            }
+        }
+
+
+        [Category("Appearance")]
+        [Description("blabla")]
+        public bool txtMultiline
+        {
+            set
+            {
+                txt.Multiline = value;
+            }
+            get
+            {
+                return txt.Multiline;
+            }
+        }
+
+        public enum eTipo
+        {
+            Numerico,
+            Textual
+        }
+
+        private eTipo tipo;
+        [Category("Appearance")]
+        [Description("enum")]
+        public eTipo Tipo
+        {
+            set
+            {
+                tipo = value;
+            }
+            get
+            {
+                return tipo;
+            }
+        }
+
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            IniciarTextBox();
+            using (Pen lapiz = new Pen(Color.Red))
+            {
+                Rectangle r = new Rectangle(5, 5, this.Width - 10, this.Height - 10);
+                e.Graphics.DrawRectangle(lapiz, r);
+            }
+        }
+
+
+    }
+}
